83369b7fb8c4b8aedec056d1e1a3540badaa5009
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-06 07:01:48 -0300

    Parametrização dos tamanhos das minas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import params from "./params";
 
-export default props => {
-    return (
-        <>
-            <SafeAreaView style={styles.App}>
-                <Text>Campo Minado</Text>
-            </SafeAreaView>
-        </>
-    )
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <SafeAreaView style={styles.container}>
+                    <Text style={styles.welcome}>Campo Minado</Text>
+                    <Text style={styles.instructions}>Tamanho da grade:
+                        {params.getRowsAmount()}x{params.getColumnsAmount()}
+                    </Text>
+                </SafeAreaView>
+            </>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-    App: {
+    container: {
         flex: 1,
-        backgroundColor:"#fff",
+        backgroundColor: "#f5fcff",
         justifyContent: "center",
         alignItems: "center",
-        padding:20
-    }
+        padding: 10
+    },
+    welcome:{
+        fontSize: 20,
+        textAling:'center',
+        margin:10,
+    },
 })
