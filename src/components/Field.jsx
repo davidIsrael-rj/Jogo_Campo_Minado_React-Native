4738459a8b82563495d9cfa7981449cad34cdd10
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-06 09:11:16 -0300

    Componente flag

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View } from "react-native"
 import params from "../params"
 import Mine from "./Mine"
+import Flag from "./Flag"
 
 export default props => {
 
-    const { mined, opened, nearMines, exploded } = props
+    const { mined, opened, nearMines, exploded, flagged } = props
 
     const styleField = [styles.field]
     if (opened) styleField.push(styles.opened)
     if (exploded) styleField.push(styles.exploded)
+    if (flagged) styleField.push(styles.flagged, styles.regular)
     if (styleField.length === 1) styleField.push(styles.regular)
 
     let color = null
@@ -25,6 +27,8 @@ export default props => {
                 <Text style={[styles.label, { color: color }]}>
                     {nearMines}</Text> : false}
                 {mined && opened ? <Mine/> : false}
+                {flagged && !opened ? <Flag/> : false}
+
         </View>
     )
 }
