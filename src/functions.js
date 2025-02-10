6c3c14b800d188ca8d7064db0b898d5de90d242f
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-10 06:32:07 -0300

    função getMeighbors

--- src/functions.js
+++ src/functions.js
@@ -46,5 +46,18 @@ const cloneBoard = board => {
     })
 }
 
-
+const getMeighbors = (board, row, column)=>{
+    const neighbors =[]
+    const rows = [row -1, row, row +1]
+    const columns = [column -1, column, column + 1]
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const diferent = r !== row || c !== column
+            const validRow = r >= 0 && c < board[0].length
+            if(diferent && validRow && validColumn) {
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+}
 export {createMinedBoard}
