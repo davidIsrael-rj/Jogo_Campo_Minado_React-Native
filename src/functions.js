bd0db101be8fdb977bfaeccabbe25f6d637035fc
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-11 07:40:07 -0300

    função invertFlag

--- src/functions.js
+++ src/functions.js
@@ -96,6 +96,12 @@ const wonGame = board => fields(board).filter(pendding).lenght === 0
 const showMines = board => fields(board).filter(field => field.mined)
     .forEach(field => field.opened = true)
 
+const invertFlag = (board, row, column) => {
+    const field = board[row][column]
+    field.flagged = !field.flagged
+}
+
+
 
 export { 
     createMinedBoard,
@@ -104,5 +110,6 @@ export {
     hadExplosion,
     wonGame,
     showMines,
+    invertFlag,
     
 }
